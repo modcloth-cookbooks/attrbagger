52048c9e4c824cdd1b4eae9d09f8afe7ae2f90d3
Dan Buch <d.buch@modcloth.com>
2013-09-01 23:01:52 -0400

    Egad, overdue version bump

--- metadata.rb
+++ metadata.rb
@@ -4,4 +4,4 @@ maintainer_email 'github+attrbagger-cookbook@modcloth.com'
 license          'MIT'
 description      'Installs/Configures attrbagger'
 long_description File.read(File.expand_path('../README.md', __FILE__))
-version          '0.1.0'
+version          '0.2.0'
